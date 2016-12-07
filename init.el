42c4af310bb1492f7c2677505c5cc717cc40e89b
condotti <akondo21@gmail.com>
2016-12-07 11:53:28 +0900

    move-file added

--- init.el
+++ init.el
@@ -255,6 +255,25 @@ Default to a pdf, or a html if ARG is not nil."
               (lambda (args)
                 (cons (encode-coding-string (car args) 'cp932) (cdr args)))))
 ;; ----------------------------------------------------------------------
+;; from http://zck.me/emacs-move-file
+;; ----------------------------------------------------------------------
+(defun move-file (new-location)
+  "Write this file to NEW-LOCATION, and delete the old one."
+  (interactive (list (if buffer-file-name
+                         (read-file-name "Move file to: ")
+                       (read-file-name "Move file to: "
+                                       default-directory
+                                       (expand-file-name (file-name-nondirectory (buffer-name))
+                                                         default-directory)))))
+  (when (file-exists-p new-location)
+    (delete-file new-location))
+  (let ((old-location (buffer-file-name)))
+    (write-file new-location t)
+    (when (and old-location
+               (file-exists-p new-location))
+      (delete-file old-location))))
+(bind-key "C-c m" #'move-file)
+;; ----------------------------------------------------------------------
 ;; Packages
 ;; ----------------------------------------------------------------------
 ;; number etc.
