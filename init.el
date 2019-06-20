1243892e42e8cd2be583fcb13ab5c8111fc61b84
condotti <akondo21@gmail.com>
2019-06-20 14:18:05 +0900

    add anthy support for chromebook

--- init.el
+++ init.el
@@ -79,8 +79,28 @@
 	       "<!-- End: -->\n"))
 
 ;; ----------------------------------------------------------------------
+;; anthy for chromebook
+;;   assumes anthy is configured with default prefix (/usr/local)
+;; ----------------------------------------------------------------------
+(when (eq system-type 'gnu/linux)
+  (let* ((anthy-prefix "/usr/local")
+	 ;; (anthy-bindir (concat anthy-prefix "/bin"))
+	 ;; (anthy-libdir (concat anthy-prefix "/lib"))
+	 (anthy-lispdir (concat anthy-prefix "/share/emacs/site-lisp/anthy")))
+    ;; (add-to-list 'exec-path anthy-bindir)
+    (add-to-list 'load-path anthy-lispdir)
+    ;; (setenv "PATH" (concat (getenv "PATH") path-separator anthy-bindir))
+    ;; (setenv "LD_LIBRARY_PATH" anthy-libdir)
+    (define-obsolete-variable-alias 'last-command-char 'last-command-event
+      "at least 19.34")
+    (load-library "anthy")
+    (load-library "leim-list")
+    (setq default-input-method 'japanese-anthy)))
+
+;; ----------------------------------------------------------------------
 ;; Packages
 ;; ----------------------------------------------------------------------
+
 (use-package counsel
   :bind (("C-x C-r" . counsel-recentf)
 	 ("C-x b" . counsel-switch-buffer))
