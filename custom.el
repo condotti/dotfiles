6bb196c4de718898b8e5009e260f58c765621f09
condotti <akondo21@gmail.com>
2021-02-11 09:46:43 +0900

    adjust mozc related parameters

--- custom.el
+++ custom.el
@@ -7,34 +7,33 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backup"))))
+ '(backup-directory-alist '((".*" . "~/.emacs.d/backup")))
  '(calendar-week-start-day 1)
- '(custom-enabled-themes (quote (deeper-blue)))
+ '(custom-enabled-themes '(deeper-blue))
  '(display-time-24hr-format t)
  '(display-time-mode t)
  '(global-display-line-numbers-mode t)
  '(global-prettify-symbols-mode t)
- '(glyphless-char-display-control
-   (quote
-    ((format-control . thin-space)
-     (no-font . empty-box))))
+ '(glyphless-char-display-control '((format-control . thin-space) (no-font . empty-box)))
  '(inhibit-startup-screen t)
- '(recentf-auto-cleanup (quote never))
+ '(jit-lock-chunk-size 20)
+ '(jit-lock-context-time 5.0)
+ '(jit-lock-defer-time 1.0)
+ '(jit-lock-stealth-load 300)
+ '(jit-lock-stealth-time 20)
+ '(recentf-auto-cleanup 'never)
  '(recentf-max-saved-items 2000)
  '(recentf-mode t)
  '(safe-local-variable-values
-   (quote
-    ((go-test-args . "-v --bench . --benchmem")
-     (eval add-hook
-	   (quote before-save-hook)
-	   (function
-	    (lambda nil
-	      (save-excursion
-		(goto-char
-		 (point-min))
-		(while
-		    (re-search-forward "max-width: *[0-9]+px" nil t)
-		  (replace-match "max-width: 300px" nil nil)))))
+   '((go-test-args . "-v --bench . --benchmem")
+     (eval add-hook 'before-save-hook
+	   #'(lambda nil
+	       (save-excursion
+		 (goto-char
+		  (point-min))
+		 (while
+		     (re-search-forward "max-width: *[0-9]+px" nil t)
+		   (replace-match "max-width: 300px" nil nil))))
 	   nil t)
      (buffer-file-coding-system . utf-8)
      (org-html-postamble-format
@@ -42,7 +41,7 @@
      (org-html-postamble . t)
      (org-html-preamble-format
       ("en" "<h1 class='mytitle'>%t (%d)</h1>"))
-     (org-html-preamble . t))))
+     (org-html-preamble . t)))
  '(save-place-mode t)
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
